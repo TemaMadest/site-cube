b48408e2b719b57504a97ca6f8dfe7f377125541
temamadest <madestoff@yandex.ru>
2018-04-25 14:20:45 +0300

    add beetle

--- src/js/beetle.js
+++ src/js/beetle.js
@@ -3,18 +3,29 @@
 	class beetle{
 
 		constructor(params) {
-		    this.x = params.x;
-		    this.y = params.y;
+		    this.x = null;
+		    this.y = null;
 		    this.width = 30;
 		    this.height = 30;
+		    this.time = 0;//time of burn
 		    this.page = params.page;
+		    this.angle = 0;//deg
+		    this.speed = 10;//px / sec
+		    this.range = 200;//px
+		    this.state = "";
 		}
 		
 		generate(){
 
 		}
 
-		
+		destroy(){
+			
+		}
+
+		burn(){
+			
+		}
 	}
 
 }());
